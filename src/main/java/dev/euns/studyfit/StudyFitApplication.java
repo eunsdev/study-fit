bed1f7a43056209a5c115907a63c2d7d3d49abed
Pudgy Lee <79688235+eunsdev@users.noreply.github.com>
2026-04-23 15:02:55 +0900

    first commit

---
+++ src/main/java/dev/euns/studyfit/StudyFitApplication.java
@@ -0,0 +1,13 @@
+package dev.euns.studyfit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyFitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyFitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/euns/studyfit/StudyFitApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.euns.studyfit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyFitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
